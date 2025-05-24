391fc556d51c292d8fab1db813c8eb2640fd77fa
Primrose <primrose.njoki@student.moringaschool.com>
2025-05-24 11:18:11 +0300

    bears

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears (name,age,sex,color,temperament,alive) VALUES('Mr. Chocolate', 10, 'M', 'brown', 'calm', 1),
+('Rowdy', 7, 'M', 'black', 'aggressive', 1),
+('Tabitha', 5, 'F', 'white', 'playful', 1),
+('Sergeant Brown', 12, 'M', 'brown', 'serious', 1),
+('Melissa', 6, 'F', 'gray', 'sweet', 1),
+('Grinch', 8, 'M', 'green', 'grumpy', 1),
+('Wendy', 4, 'F', 'blonde', 'cheerful', 1),
+(NULL, 3, 'F', 'black', 'shy', 1);
